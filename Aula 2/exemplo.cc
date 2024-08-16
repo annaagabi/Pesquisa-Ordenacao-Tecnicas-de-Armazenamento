70bfa42a6b76670a26b4e2ae8777326f4108b012
annaagabi <annaa0478@gmail.com>
2024-08-16 11:13:54 -0300

    Feat: aula 2 - tudo o restante do material

--- Aula 2/exemplo.cc
+++ Aula 2/exemplo.cc
@@ -7,13 +7,22 @@ int main(int argc, char * argv[]{
     printf("Digiten:");     // 1
     scanf("%d", &n);        // 1
 
+    // for(i=0; i<n; i++){
+    //     for(j=0; j<n; j++){
+    //         x += 1;         // n^2
+    //     }
+    // }
+
     for(i=0; i<n; i++){
         for(j=0; j<n; j++){
-            x += 1;         // n^2
+            for(k=0; k<n; k++){  
+            x += 1;         // n^3
+            }
         }
     }
 
     printf("x = %d", x)     // 1
 
     return 0;               // 1
-})                          // n^2 +5
+// })                          // n^2 +5
+})                          // n^3 +5
